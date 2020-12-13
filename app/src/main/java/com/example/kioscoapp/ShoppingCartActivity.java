78b3f98538c36e9f487658c8f6ee2e9848011aab
unknown <aroche.aylin@gmail.com>
2020-12-13 14:05:12 -0600

    Add shopping cart activity

--- app/src/main/java/com/example/kioscoapp/ShoppingCartActivity.java
+++ app/src/main/java/com/example/kioscoapp/ShoppingCartActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -32,13 +33,22 @@ public class ShoppingCartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_shopping_cart);
 
         lvShoppingCart = (ListView) findViewById(R.id.listShopingCart);
+        Button button= (Button) findViewById(R.id.btnGenerateCode);
+
         lvShoppingCart.setDivider(null);
         lvShoppingCart.setAdapter(new CartAdapter(this, data));
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateCode(v);
+            }
+        });
     }
+
 
     public void generateCode(View view){
         Intent goPaymentCode = new Intent(this, PaymentCodeActivity.class);
