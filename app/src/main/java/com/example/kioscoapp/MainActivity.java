e70b5d803d0ef20ea4c9ab56062b95efe7ac6968
Kimberly <kimberly.morales.arias@gmail.com>
2020-12-13 09:51:52 -0600

    listView providers

--- app/src/main/java/com/example/kioscoapp/MainActivity.java
+++ app/src/main/java/com/example/kioscoapp/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.kioscoapp;
 
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    public static final String SELECTED_INDEX = "";
+    ListView list;
+    String selectedIndex = "0";
 
+    String[] maintitle ={
+            "Kolbi CR","AYA", "Claro"
+    };
+
+    Integer[] imgid={
+            R.drawable.download_1,R.drawable.download_2,
+            R.drawable.download_3,R.drawable.download_1,
+            R.drawable.download_2,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyListView adapter = new MyListView(this, maintitle,imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+                // TODO Auto-generated method stub
+                    //code specific to first list item
+                    selectedIndex = maintitle[position];
+                    sendMessage(view);
+                    Toast.makeText(getApplicationContext(),maintitle[position],Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, SelectedProviderActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+       // String message = editText.getText().toString();
+        intent.putExtra(SELECTED_INDEX, selectedIndex);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/kioscoapp/MyListView.java
@@ -0,0 +1,40 @@
+package com.example.kioscoapp;
+
+import android.app.Activity;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class MyListView extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] maintitle;
+    private final Integer[] imgid;
+
+    public MyListView(Activity context, String[] maintitle, Integer[] imgid) {
+        super(context, R.layout.mylist, maintitle);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.maintitle=maintitle;
+        this.imgid=imgid;
+
+    }
+
+    public View getView(int position,View view,ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.mylist, null,true);
+
+        TextView titleText = (TextView) rowView.findViewById(R.id.title);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+
+        titleText.setText(maintitle[position]);
+        imageView.setImageResource(imgid[position]);
+        return rowView;
+
+    };
+}
