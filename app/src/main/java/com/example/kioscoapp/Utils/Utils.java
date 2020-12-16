086bd482c34fce906dbc849c200ab9afca8a7d14
Kimberly <kimberly.morales.arias@gmail.com>
2020-12-16 14:44:24 -0600

    Format dates

---
+++ app/src/main/java/com/example/kioscoapp/Utils/Utils.java
@@ -0,0 +1,12 @@
+package com.example.kioscoapp.Utils;
+
+public class Utils {
+
+        public static String getMonthOfTheYear(int i){
+            String[] months = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre"
+                    , "Noviembre", "Diciembre"};
+
+            return months[i];
+        }
+  
+}
