441aa87af72259ed63281df2ceba1d3fb04641aa
Kimberly <kimberly.morales.arias@gmail.com>
2020-12-21 10:38:49 -0600

    fix printer configuration

--- app/src/main/java/com/example/kioscoapp/PaymentCodeActivity.java
+++ app/src/main/java/com/example/kioscoapp/PaymentCodeActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Bundle;
-
-
-import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.kioscoapp.R;
 import com.toshiba.tgcsapi.AppContext;
 import com.toshiba.tgcsapi.POS;
 import com.toshiba.tgcsapi.POSDataEvent;
@@ -22,16 +28,6 @@ import com.toshiba.tgcsapi.TGCSMPOS4610Printer;
 import com.toshiba.tgcsapi.TGCSMPOSException;
 import com.toshiba.tgcsapi.TGCSMPOSLogging;
 import com.toshiba.util.DeviceInfo;
-import android.util.Base64;
-import android.util.Log;
-import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
@@ -39,13 +35,14 @@ public class PaymentCodeActivity extends AppCompatActivity {
 
     WebView webViewService,webViewRecharge;
     Button buttonPrint;
+    TextView textView7,textView9;
     boolean error;
     public static final String LOGFILE_PATH =
             Environment.getExternalStorageDirectory().getPath().toString()+
                     "/MobileSDKTestApp/logs/";
     private static TGCSMPOS4610Printer device = null;
     private boolean initializationFlag = true;
-
+    Bitmap codigoBarras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,148 +55,139 @@ public class PaymentCodeActivity extends AppCompatActivity {
         webViewService=findViewById(R.id.webViewService);
         webViewRecharge=findViewById(R.id.webViewRecharge);
         buttonPrint=findViewById(R.id.buttonReadyPrint);
-
+        textView7 = findViewById(R.id.textView7);
+        textView9 = findViewById(R.id.textView9);
         paintCodeBar(webViewService,codService);
         paintCodeBar(webViewRecharge,codRecharge);
 
+        AppContext.getInstance().setContext(this);
         DeviceInfo trace = new DeviceInfo(this);
         trace.printDeviceInformation(); // Capture device hardware and manufacturer information
         initializeDriverLogs();
+        initializeUI();
 
-        buttonPrint.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                print();
-            }
-        });
     }
 
-    private void  print(){
-        Log.i("DemoApp", "OnClick button pressed");
-        String localDeviceName = "POSPrinter:USB";
-        error=false;
-        try
-        {
-            if (initializationFlag == true){
-                device = new TGCSMPOS4610Printer();
-                // add POSEventHandlers. This allows to receive notifications for:
-                // Paper status among other notifications.
-                device.addPOSEventListener(new POSEventHandler() {
-                    /**
-                     * There are additional event handlers for Data, Errors and OutputComple
-                     * Events. Please refer to UPOS standard for additional information on
-                     * how to use them:
-                     *  - https://www.omg.org/cgi-bin/doc?retail/2017-07-32
-                     */
-                    @Override
-                    public void handleEvent(POSEvent posEvent) {
-
-                    }
-
-                    @Override
-                    public void handleDataEvent(POSDataEvent posDataEvent) {
-
-                    }
-
-                    @Override
-                    public void handleErrorEvent(POSErrorEvent posErrorEvent) {
-
-                    }
-
-                    @Override
-                    public void handleOutputCompleteEvent(POSOutputCompleteEvent posOutputCompleteEvent) {
-
-                    }
-
-                    /**
-                     * A StatusUpdateEvent(SUE) is an object that represent a physical change
-                     * in the device. The following are examples of SUE: cover position, paper
-                     * status, device power.
-                     *
-                     * Implement "POSEventHandler" interface and Use this method to process
-                     * StatusUpdateEvents.
-                     *
-                     * @param sue: StatusUpdateEvent notification/object.
-                     */
-                    @Override
-                    public void handleStatusUpdateEvent(POSStatusUpdateEvent sue) {
-
-                        switch(sue.getStatus())
-                        {
-                            /*
-                             * StatusUpdateEvents for the covers.
+    private void initializeUI() {
+        final Button btn_print = (Button) findViewById(R.id.buttonReadyPrint);
+        btn_print.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("DemoApp", "OnClick button pressed");
+                String localDeviceName = "POSPrinter:USB";
+                try
+                {
+                    if (initializationFlag == true){
+                        device = new TGCSMPOS4610Printer();
+                        // add POSEventHandlers. This allows to receive notifications for:
+                        // Paper status among other notifications.
+                        device.addPOSEventListener(new POSEventHandler() {
+                            /**
+                             * There are additional event handlers for Data, Errors and OutputComple
+                             * Events. Please refer to UPOS standard for additional information on
+                             * how to use them:
+                             *  - https://www.omg.org/cgi-bin/doc?retail/2017-07-32
                              */
-                            case POS.PTR_SUE_COVER_OPEN:
-                                Log.i("STATUS", "PTR_SUE_COVER_OPEN");
-                                break;
-                            case POS.PTR_SUE_COVER_OK:
-                                Log.i("STATUS", "PTR_SUE_COVER_OK");
-                                break;
-                            case POS.PTR_SUE_REC_COVER_OPEN:
-                                Log.i("STATUS", "PTR_SUE_REC_COVER_OPEN");
-                                break;
-                            case POS.PTR_SUE_REC_COVER_OK:
-                                Log.i("STATUS", "PTR_SUE_REC_COVER_OK");
-                                break;
-                            /*
-                             * StatusUpdateEvents for the paper.
-                             * REC stands for "Receipt" and that is the thermal station.
-                             * SLP stands for "Slip" and that is the impact station.
+                            @Override
+                            public void handleEvent(POSEvent posEvent) {
+
+                            }
+
+                            @Override
+                            public void handleDataEvent(POSDataEvent posDataEvent) {
+
+                            }
+
+                            @Override
+                            public void handleErrorEvent(POSErrorEvent posErrorEvent) {
+
+                            }
+
+                            @Override
+                            public void handleOutputCompleteEvent(POSOutputCompleteEvent posOutputCompleteEvent) {
+
+                            }
+
+                            /**
+                             * A StatusUpdateEvent(SUE) is an object that represent a physical change
+                             * in the device. The following are examples of SUE: cover position, paper
+                             * status, device power.
+                             *
+                             * Implement "POSEventHandler" interface and Use this method to process
+                             * StatusUpdateEvents.
+                             *
+                             * @param sue: StatusUpdateEvent notification/object.
                              */
-                            case POS.PTR_SUE_REC_EMPTY:
-                                Log.i("STATUS", "PTR_SUE_REC_EMPTY");
-                                break;
-                            case POS.PTR_SUE_REC_PAPEROK:
-                                Log.i("STATUS", "PTR_SUE_REC_PAPEROK");
-                                break;
-                            case POS.PTR_SUE_SLP_EMPTY:
-                                Log.i("STATUS", "PTR_SUE_SLP_EMPTY");
-                                break;
-                            case POS.PTR_SUE_SLP_PAPEROK:
-                                Log.i("STATUS", "PTR_SUE_SLP_PAPEROK");
-                                break;
-                            default:
-                                // there are other SUEs.
-                                // Identifier of symbolic constants for all of them follow
-                                // the pattern: PTR_SUE_*
-                                Log.i("STATUS", "Status update value : " + sue.getStatus());
-                        }
+                            @Override
+                            public void handleStatusUpdateEvent(POSStatusUpdateEvent sue) {
+
+                                switch(sue.getStatus())
+                                {
+                                    /*
+                                     * StatusUpdateEvents for the covers.
+                                     */
+                                    case POS.PTR_SUE_COVER_OPEN:
+                                        Log.i("STATUS", "PTR_SUE_COVER_OPEN");
+                                        break;
+                                    case POS.PTR_SUE_COVER_OK:
+                                        Log.i("STATUS", "PTR_SUE_COVER_OK");
+                                        break;
+                                    case POS.PTR_SUE_REC_COVER_OPEN:
+                                        Log.i("STATUS", "PTR_SUE_REC_COVER_OPEN");
+                                        break;
+                                    case POS.PTR_SUE_REC_COVER_OK:
+                                        Log.i("STATUS", "PTR_SUE_REC_COVER_OK");
+                                        break;
+                                    /*
+                                     * StatusUpdateEvents for the paper.
+                                     * REC stands for "Receipt" and that is the thermal station.
+                                     * SLP stands for "Slip" and that is the impact station.
+                                     */
+                                    case POS.PTR_SUE_REC_EMPTY:
+                                        Log.i("STATUS", "PTR_SUE_REC_EMPTY");
+                                        break;
+                                    case POS.PTR_SUE_REC_PAPEROK:
+                                        Log.i("STATUS", "PTR_SUE_REC_PAPEROK");
+                                        break;
+                                    case POS.PTR_SUE_SLP_EMPTY:
+                                        Log.i("STATUS", "PTR_SUE_SLP_EMPTY");
+                                        break;
+                                    case POS.PTR_SUE_SLP_PAPEROK:
+                                        Log.i("STATUS", "PTR_SUE_SLP_PAPEROK");
+                                        break;
+                                    default:
+                                        // there are other SUEs.
+                                        // Identifier of symbolic constants for all of them follow
+                                        // the pattern: PTR_SUE_*
+                                        Log.i("STATUS", "Status update value : " + sue.getStatus());
+                                }
+
+                            }
+                        });
+
+                        // open the device
+                        device.open(localDeviceName);
+
+                        // claim the device for exclusive access; 3000ms timeout
+                        device.claim(3000);
+
+                        // enable the device
+                        device.setDeviceEnabled(true);
+
+                        initializationFlag = false;
 
                     }
-                });
+                    System.out.println("***DEVICE PAPER: "+device.getRecEmpty());
+                    System.out.println("***DEVICE COVER: "+device.getCoverOpen());
 
-
-                // open the device
-                device.open(localDeviceName);
-
-                // claim the device for exclusive access; 3000ms timeout
-                device.claim(3000);
-
-                // enable the device
-                device.setDeviceEnabled(true);
-
-                initializationFlag = false;
-
+                    PrinterReceipt(localDeviceName);
+                }
+                catch( Exception e )
+                {
+                    e.printStackTrace();
+                    Log.d("DemoApp", "Exception: " + e.getMessage());
+                }
             }
-            System.out.println("***DEVICE PAPER: "+device.getRecEmpty());
-            System.out.println("***DEVICE COVER: "+device.getCoverOpen());
-
-            PrinterReceipt(localDeviceName);
-        }
-        catch (NullPointerException e){
-            if(!error){
-                Toast.makeText(this,"Error al imprimir: Asegurese que la impresora este conectada",Toast.LENGTH_LONG).show();
-                error=true;
-            }
-        }
-        catch( Exception e )
-        {
-
-            e.printStackTrace();
-            Log.d("DemoApp", "Exception: " + e.getMessage());
-        }catch (UnsatisfiedLinkError err){
-            Toast.makeText(this,"Error al imprimir: Asegurese que la impresora este conectada",Toast.LENGTH_LONG).show();
-        }
+        });
     }
 
 
@@ -210,22 +198,16 @@ public class PaymentCodeActivity extends AppCompatActivity {
      * @effects: prints sample receipt on the device.
      * @return: String result
      */
-    private void PrinterReceipt(String logicalDeviceName)
-    {
+    private void PrinterReceipt(String logicalDeviceName) {
         // Receipt content
-        String content = "\\x1B|N\\x1B|bC\\x1B|3C\\x1B|cATICKET STORE\n";
+        String content = "\\x1B|N\\x1B|bC\\x1B|3C\\x1B|cACarrito Servicios\n";
         content +=       "\\x1B|N\\x1B|cAMMM/DD/YY  HH:MM\n\n";
         content +=       "\\x1B|cA\\x1B|bCStore Number:\\x1B|N 8888\n";
-        content +=       "\\x1B|cA\\x1B|bCSeller:\\x1B|N Your Name\n\n";
+        content +=       "\\x1B|cA\\x1B|bCSeller:\\x1B|N Kimberly Morales\n\n";
         content +=       "\\x1B|cAItem 1 .................... $20\n";
-        content +=       "\\x1B|cAItem 2 .................... $30\n";
-        content +=       "\\x1B|cAItem 3 .................... $40\n";
-        content +=       "\\x1B|cAItem 4 .................... $50\n";
-        content +=       "\\x1B|cAItem 5 .................... $60\n";
-        content +=       "\\x1B|cAItem 6 .................... $70\n";
         content +=       "\\x1B|cAItem 7 .................... $80\n";
         content +=       "\n";
-        content +=       "\\x1B|cA\\x1B|3C\\x1B|bCAmount .................... $350\n";
+        content +=       "\\x1B|cA\\x1B|3C\\x1B|bCAmount .................... $100\n";
         content +=       "\\x1B|cA\\x1B|NCreditCard (####-####-####-####)\n";
         content +=       "\\x1B|cATAXES INCLUDED\n\n";
 
@@ -323,13 +305,29 @@ public class PaymentCodeActivity extends AppCompatActivity {
             String textHtml="<div width='100%'><img style='display:block;margin:auto' src='"+base64+"'></img></div>";
             if(!base64.isEmpty()){
                 webView.loadData(textHtml,"text/html","UTF-8");
+                codigoBarras =  screenshot(webView);
             }
         }catch (Exception e){
 
         }
 
     }
+    private static Bitmap screenshot(WebView webView) {
+        webView.measure(View.MeasureSpec.makeMeasureSpec(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED),View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        webView.layout(0, 0, webView.getMeasuredWidth(),
+                webView.getMeasuredHeight());
+        webView.setDrawingCacheEnabled(true);
+        webView.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(webView.getMeasuredWidth(),
+                webView.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
 
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        int iHeight = bitmap.getHeight();
+        canvas.drawBitmap(bitmap, 0, iHeight, paint);
+        webView.draw(canvas);
+        return bitmap;
+    }
     public  void back(View view){
         finish();
     }
